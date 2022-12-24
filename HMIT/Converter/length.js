7cb39cfbc6ff2afc65628be535ed62393b55b0f3
ZainulAbdeen-101 <bbadshah691@gmail.com>
2022-12-24 20:49:31 +0500

    done

--- HMIT/Converter/length.js
+++ HMIT/Converter/length.js
@@ -9,16 +9,27 @@
 //  });
 
 $(document).ready(function () {
-  $("#k").click(function () {
-;
+  dd = $("#drop").val();
+  dt = $("#drop1").val();
+
+  $("#drop").change(function () {
     dd = $("#drop").val();
-    dt=$("#drop1").val()
-    if (dd == "inch" && dt== "millimeter") {
-     
+    dt = $("#drop1").val();
+   
+  });
+
+  $("#drop1").change(function () {
+    dd = $("#drop").val();
+    dt = $("#drop1").val();
+    console.log(dt);
+  });
+
+  $("#inp1").on("input", function () {
+    if (dd == "inch" && dt == "millimeter") {
       var value1 = $("#inp1").val();
       let and = value1 * 25.4;
       $("#go").val(and);
-    } else if (dd == "millimeter" && dt=="inch") {
+    } else if (dd == "millimeter" && dt == "inch") {
       var value1 = $("#inp1").val();
       let and1 = value1 / 25.4;
       $("#go").val(and1);
